67359e5c625d420ad78df81dd14303846184e850
kishlay <kishlay141@gmail.com>
2025-06-13 12:26:39 +0530

    completed User.js

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  balance: { type: Number, default: 0 },
+  transactions: [{ type: mongoose.Schema.Types.ObjectId, ref: "Transaction" }],
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.correctPassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model("User", userSchema);
